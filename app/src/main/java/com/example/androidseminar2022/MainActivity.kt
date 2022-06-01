89f741693efeeb909da1fff874e33da67435e915
Nguyen Van Phuc <nguyenvanphuc0497@gmail.com>
2022-06-01 21:31:09 +0700

    chuc nang 07 tao conflict

--- app/src/main/java/com/example/androidseminar2022/MainActivity.kt
+++ app/src/main/java/com/example/androidseminar2022/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        chucNang01()
+        chucNang02()
     }
 
     private fun chucNang01(){
